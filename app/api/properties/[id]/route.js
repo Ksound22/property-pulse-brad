14ebc44a907fd604b41e37f00d748b977230e2d3
kolade <chrisjay967@gmail.com>
2024-04-22 10:42:41 +0100

    creted single property page

---
+++ app/api/properties/[id]/route.js
@@ -0,0 +1,21 @@
+import connecDB from '@/config/database';
+import Property from '@/models/Property';
+
+// GET /api/properties/:id
+export const GET = async (request, { params }) => {
+  try {
+    await connecDB();
+
+    const property = await Property.findById(params.id);
+
+    if (!property) {
+      return new Response('Property not found!', { status: 404 });
+    }
+
+    return new Response(JSON.stringify(property), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response('Something went wrong', { status: 500 });
+  }
+};
